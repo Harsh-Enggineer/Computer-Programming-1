0727a701fd2343f8db7e217b994e4fe0b00c6630
Harsh Dabhi <harshdabhi6107@gmail.com>
2025-08-13 16:17:59 +0530

    Add files via upload

---
+++ Even or Odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter Number");
+    int number;
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+    printf("Number Is Even");
+    }
+    else
+    {
+    printf("Number Is Odd");
+    }
+    return 0;
+}
