f6b78d5c637abce434aaf9468afd15005782010f
Harsh Dabhi <harshdabhi6107@gmail.com>
2025-08-16 03:01:34 -0700

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enrer 1 No");
+    scanf("%d",&a);
+    printf("Enter 2 No");
+    scanf("%d",&b);
+    c=a-b;
+    printf("%d-%d=%d",a,b,c);
+}
