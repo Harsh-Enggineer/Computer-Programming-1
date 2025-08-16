0240c645ee71c7f68cbc4884151273bbc0fbf3de
Harsh Dabhi <harshdabhi6107@gmail.com>
2025-08-16 05:31:23 -0700

    Add files via upload

---
+++ div.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter First No");
+    scanf("%d",&a);
+    printf("Enter Second No");
+    scanf("%d",&b);
+    c=a/b;
+    printf("%d/%d=%d",a,b,c);
+}
